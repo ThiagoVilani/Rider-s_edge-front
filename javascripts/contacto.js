546774cfa96a41549f94555085499e16c8a88918
ThiagoVilani <thiagovilani19@gmail.com>
2025-02-11 11:50:34 -0300

    Elimino archivos inutiles

--- javascripts/contacto.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    document.getElementsByClassName("btn-opcion-nav")[0].addEventListener("click",function(){
-        window.location.href = ".productos-listados.html";
-    });
-});
